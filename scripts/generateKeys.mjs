f15493b4e01266c8b7f7b34f7db2485e976aa17c
naman7727 <namankhandelwal7727@gmail.com>
2025-07-20 00:04:23 +0530

    add this file

--- scripts/generateKeys.mjs
+++ scripts/generateKeys.mjs
@@ -1,24 +1,21 @@
 /* eslint-disable no-undef */
-import crypto from 'crypto';
-import fs from 'fs';
+import crypto from "crypto";
+import fs from "fs";
 
-const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 2048,
-    publicKeyEncoding: {
-        type: 'pkcs1',  
-        format: 'pem'
-    },
-    privateKeyEncoding: {  
-        type: 'pkcs1',
-        format: 'pem',
-        
-    }
+const { privateKey, publicKey } = crypto.generateKeyPairSync("rsa", {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: "pkcs1",
+    format: "pem",
+  },
+  privateKeyEncoding: {
+    type: "pkcs1",
+    format: "pem",
+  },
 });
 
 console.log("Public Key:\n", publicKey);
 console.log("Private Key:\n", privateKey);
 
-
-fs.writeFileSync('certs/private.pem', privateKey);
-fs.writeFileSync('certs/public.pem', publicKey);
-
+fs.writeFileSync("certs/private.pem", privateKey);
+fs.writeFileSync("certs/public.pem", publicKey);
