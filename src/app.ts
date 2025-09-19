5baac217d1b474cebd6bc064e8d31bf922b393d9
naman7727 <namankhandelwal7727@gmail.com>
2025-09-19 10:44:00 +0530

    add create teanat endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
 // import express from "express";
-import path from "path";
+// import path from "path";
 import "reflect-metadata";
 import { HttpError } from "http-errors";
 // import logger from "./config/logger";
 import cookieParser from "cookie-parser";
 import express, { NextFunction, Request, Response } from "express";
 import authRouter from "./routes/auth";
+import tenantRouter from "./routes/tenant";
 const app = express();
 
-app.use(express.json());
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("../public"));
 app.use(cookieParser());
-app.use("/auth", authRouter);
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Auth service from K8s");
 });
-
+app.use("/auth", authRouter);
+app.use("/tenants", tenantRouter);
 // Add this route handler to solve the error
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -40,3 +40,4 @@ export default app;
 // app.get("/", (req, res) => {
 //   res.send("Welcome to Auth service from K8s");
 // });
+//  docker run --rm --name mernpg-container -e POSTGRES_USER=root -e POSTGRES_PASSWORD=root -v mernpgdata:/var/lib/postgresql/data -p 5432:5432 -d postgres
---
+++ src/routes/tenant.ts
@@ -0,0 +1,14 @@
+import { AppDataSource } from "../config/data-source";
+import { Tenant } from "../entity/Tenant";
+import { TenantController } from "./../controllers/TenantController";
+import { TenantService } from "./../services/TenantService";
+import express from "express";
+import logger from "../config/logger";
+
+const router = express.Router();
+const tenantRepository = AppDataSource.getRepository(Tenant);
+const tenantService = new TenantService(tenantRepository);
+const tenantController = new TenantController(tenantService, logger);
+
+router.post("/", (req, res, next) => tenantController.create(req, res, next));
+export default router;
